ff00c152451e26f3024d8311b94b538ad2776383
Magnus Burenius <magnus.burenius@gmail.com>
2023-07-17 22:10:48 +0200

    refactor: remove semi-colons from end of macros

--- src/carma.h
+++ src/carma.h
@@ -5,18 +5,18 @@
 #define INIT_RANGE(range, size_) do { \
     (range).data = malloc((size_) * sizeof(*(range).data)); \
     (range).size = (size_); \
-} while (0);
+} while (0)
 
 #define INIT_DARRAY(range, size_, capacity_) do { \
     (range).data = malloc((capacity_) * sizeof(*(range).data)); \
     (range).size = (size_); \
     (range).capacity = (capacity_); \
-} while (0);
+} while (0)
 
 #define FREE_RANGE(range) do { \
     free((range).data); \
     (range).data = NULL; \
-} while (0);
+} while (0)
 
 #define FOR_RANGE(it, range) \
     for (__auto_type it = (range).data; it != (range).data + (range).size; ++it)
@@ -25,9 +25,9 @@
     if ((range).size >= (range).capacity) exit(EXIT_FAILURE); \
     *((range).data + (range).size) = (value); \
     (range).size++; \
-} while (0);
+} while (0)
 
-#define CLEAR_RANGE(range) do {(range).size = 0;} while (0);
+#define CLEAR_RANGE(range) do {(range).size = 0;} while (0)
 
 // Requires GNUC:
 #define SUM_RANGE(range) ({ \
