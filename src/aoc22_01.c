f72ce1545cea8d5a01242fc7eccfdd2f8ae11f9e
Magnus Burenius <magnus.burenius@gmail.com>
2023-07-17 16:56:22 +0200

    add axample aoc22_01

---
+++ src/aoc22_01.c
@@ -0,0 +1,43 @@
+#include <math.h>
+#include <stdio.h>
+#include "carma.h"
+
+typedef struct {
+    int* data;
+    int size;
+    int capacity;
+} Ints;
+
+int main(int argc, char **argv) {
+    let file_path = argv[1];
+    printf(file_path);
+    let file = fopen(file_path, "r");
+    char buffer[256];
+    
+    Ints words;
+    INIT_DARRAY(words, 0, 10000);
+    int max_sum = 0;
+    
+    while (fgets(buffer, sizeof(buffer), file) != NULL) {
+        printf("%s", buffer);
+        let n = atoi(buffer);
+        if (n == 0) {
+            // sum values
+            int sum = 0;
+            FOR_RANGE(it, words) {
+                sum += *it;
+            }
+            max_sum = fmax(max_sum, sum);
+            words.size = 0;
+        } else {
+            APPEND(words, n);
+        }
+    }
+    fclose(file);
+    
+    printf("Max sum: %d", max_sum);
+
+    FREE_RANGE(words);
+    
+    return 0;
+}
--- src/carma.h
+++ src/carma.h
@@ -1,8 +1,16 @@
+#include <stdlib.h>
+
 #define let __auto_type // Requires GNUC. C23 also has auto.
 
 #define INIT_RANGE(range, size_) do { \
     (range).data = malloc((size_) * sizeof(*(range).data)); \
     (range).size = (size_); \
+} while (0);
+
+#define INIT_DARRAY(range, size_, capacity_) do { \
+    (range).data = malloc((capacity_) * sizeof(*(range).data)); \
+    (range).size = (size_); \
+    (range).capacity = (capacity_); \
 } while (0);
 
 #define FREE_RANGE(range) do { \
@@ -12,3 +20,9 @@
 
 #define FOR_RANGE(it, range) \
     for (__auto_type it = (range).data; it != (range).data + (range).size; ++it)
+
+#define APPEND(range, value) do { \
+    if ((range).size >= (range).capacity) exit(EXIT_FAILURE); \
+    *((range).data + (range).size) = (value); \
+    (range).size++; \
+} while (0);
