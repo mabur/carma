e3c00991d78dacadf88902c2c2a4f07c79f0f4d7
Magnus Burenius <magnus.burenius@gmail.com>
2024-03-09 12:47:58 +0100

    refactor: reorder macros and document types

--- src/carma.h
+++ src/carma.h
@@ -5,10 +5,19 @@
 #define RANGE(type) struct {type* first; type* last;}
 #define DARRAY(type) struct {type* first; type* last; type* last_allocated;}
 
+#define COUNT(range) ((range).last - (range).first)
+#define CAPACITY(darray) ((darray).last_allocated - (darray).first)
+
 #define INIT_RANGE(range, count) do { \
     (range).first = malloc((count) * sizeof(*(range).first)); \
     (range).last = (range).first + (count); \
 } while (0);
+
+#define INIT_DARRAY(darray, count, capacity) do { \
+    (darray).first = malloc((capacity) * sizeof(*(darray).first)); \
+    (darray).last = (darray).first + (count);      \
+    (darray).last_allocated = (darray).first + (capacity); \
+} while (0)
 
 #define FREE_RANGE(range) do { \
     free((range).first); \
@@ -16,35 +25,29 @@
     (range).last = NULL; \
 } while (0);
 
-#define FOR_EACH(it, range) \
-    for (__auto_type it = (range).first; it != (range).last; ++it)
-
-#define INIT_DARRAY(range, count, capacity) do { \
-    (range).first = malloc((capacity) * sizeof(*(range).first)); \
-    (range).last = (range).first + (count);      \
-    (range).last_allocated = (range).first + (capacity); \
-} while (0)
-
-#define FREE_DARRAY(range) do { \
-    free((range).first); \
-    (range).first = NULL; \
-    (range).last = NULL; \
-    (range).last_allocated = NULL; \
+#define FREE_DARRAY(darray) do { \
+    free((darray).first); \
+    (darray).first = NULL; \
+    (darray).last = NULL; \
+    (darray).last_allocated = NULL; \
 } while (0);
 
-#define APPEND(range, value) do { \
-    if ((range).last == (range).last_allocated) { \
-        size_t old_count = COUNT(range); \
-        size_t old_capacity = CAPACITY(range); \
+#define APPEND(darray, value) do { \
+    if ((darray).last == (darray).last_allocated) { \
+        size_t old_count = COUNT(darray); \
+        size_t old_capacity = CAPACITY(darray); \
         size_t new_capacity = old_capacity == 0 ? 1 : 2 * old_capacity; \
-        size_t item_size = sizeof(*(range).first); \
-        (range).first = realloc((range).first, new_capacity * item_size); \
-        (range).last = (range).first + old_count; \
-        (range).last_allocated = (range).first + new_capacity; \
+        size_t item_size = sizeof(*(darray).first); \
+        (darray).first = realloc((darray).first, new_capacity * item_size); \
+        (darray).last = (darray).first + old_count; \
+        (darray).last_allocated = (darray).first + new_capacity; \
     } \
-    *(range).last = (value); \
-    (range).last++; \
+    *(darray).last = (value); \
+    (darray).last++; \
 } while (0)
+
+#define FOR_EACH(it, range) \
+    for (__auto_type it = (range).first; it != (range).last; ++it)
 
 #define CLEAR(range) do {(range).last = (range).first;} while (0)
 
@@ -83,16 +86,13 @@
     it; \
 })
 
+// Requires GNUC:
 #define FIND_IF_BACKWARDS(range, predicate) ({ \
     let it = (range).last - 1; \
     for (; it != (range).first - 1 && !(predicate)(*it); --it) { \
     } \
     it; \
 })
-
-#define COUNT(range) ((range).last - (range).first)
-
-#define CAPACITY(range) ((range).last_allocated - (range).first)
 
 // Requires GNUC:
 #define DROP_WHILE(range, predicate) ({ \
@@ -126,18 +126,19 @@
     result; \
 })
 
-#define ERASE_IF(darray, predicate) do { \
-    let range = (darray); \
-    while (COUNT(range) > 0) { \
-        range = DROP_UNTIL(range, predicate); \
-        range = DROP_BACK_WHILE(range, predicate); \
-        if (COUNT(range) > 1) { \
-            *range.first = *(range.last - 1); \
-            ++range.first; \
-            --range.last; \
+// Requires GNUC:
+#define ERASE_IF(range, predicate) do { \
+    let sub_range = (range); \
+    while (COUNT(sub_range) > 0) { \
+        sub_range = DROP_UNTIL(sub_range, predicate); \
+        sub_range = DROP_BACK_WHILE(sub_range, predicate); \
+        if (COUNT(sub_range) > 1) { \
+            *sub_range.first = *(sub_range.last - 1); \
+            ++sub_range.first; \
+            --sub_range.last; \
         } \
     } \
-    (darray).last = range.last; \
+    (range).last = sub_range.last; \
 } while (0);
 
 #define FOR_LINES(line, capacity, file) for (char line[capacity]; fgets(line, (capacity), (file)) != NULL;)
