ea9cdd490c7397187fa8d8796906da44e919b30b
Magnus Burenius <magnus.burenius@gmail.com>
2024-03-10 00:02:37 +0100

    Update aoc22_01.c

--- src/aoc22_01.c
+++ src/aoc22_01.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 #include "carma.h"
 
+typedef struct {
+    int* data;
+    size_t count;
+    size_t capacity;
+} Ints;
+
 int main(int argc, char **argv) {
     let file_path = argv[1];
-    DARRAY(int) current_calories;
-    DARRAY(int) all_calories;
-    INIT_DARRAY(current_calories, 0, 10000);
-    INIT_DARRAY(all_calories, 0, 10000);
+    let current_calories = (Ints){};
+    let all_calories = (Ints){};
     let file = fopen(file_path, "r");
     FOR_LINES(line, 255, file) {
         let n = atoi(line);
@@ -19,8 +23,6 @@ int main(int argc, char **argv) {
     }
     fclose(file);
     let it = MAX_ELEMENT(all_calories);
-    FREE_DARRAY(current_calories);
-    FREE_DARRAY(all_calories);
     printf("Max sum: %d", *it);
     return 0;
 }
