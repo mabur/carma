9d68b3ad2858638ee1f86940ee4e27d36207f194
Magnus Burenius <magnus.burenius@gmail.com>
2024-05-04 10:44:26 +0200

    refactor: remove gnuc comments

--- carma/carma_gnu.h
+++ carma/carma_gnu.h
@@ -5,14 +5,12 @@
 ////////////////////////////////////////////////////////////////////////////////
 // ALGORITHMS FOR ALLOCATION AND FREE
 
-// Requires GNUC:
 #define MAKE_RANGE(value_type, ...) ({ \
     RANGE(value_type) result; \
     INIT_RANGE_ELEMENTS(result, __VA_ARGS__); \
     result; \
 })
 
-// Requires GNUC:
 #define MAKE_DARRAY(value_type, ...) ({ \
     DARRAY(value_type) result; \
     INIT_DARRAY_ELEMENTS(result, __VA_ARGS__); \
@@ -22,7 +20,6 @@
 ////////////////////////////////////////////////////////////////////////////////
 // RANGE ALGORITHMS
 
-// Requires GNUC:
 #define SUM(range) ({ \
     typeof(*(range).data) sum = 0; \
     FOR_EACH(it, (range)) { \
@@ -31,7 +28,6 @@
     sum; \
 })
 
-// Requires GNUC:
 #define MIN_ELEMENT(range) ({ \
     auto minimum = (range).data; \
     FOR_EACH(it, (range)) { \
@@ -40,7 +36,6 @@
     minimum; \
 })
 
-// Requires GNUC:
 #define MAX_ELEMENT(range) ({ \
     auto maximum = (range).data; \
     FOR_EACH(it, (range)) { \
@@ -49,7 +44,6 @@
     maximum; \
 })
 
-// Requires GNUC:
 #define EQUAL_RANGES(left_range, right_range) ({ \
     auto result = true; \
     if ((left_range).count != (right_range).count) { \
@@ -65,7 +59,6 @@
     result; \
 })
 
-// Requires GNUC:
 #define FIND_IF(range, predicate) ({ \
     auto it = (range).data; \
     for (; it != END_POINTER(range) && !(predicate)(*it); ++it) { \
@@ -73,7 +66,6 @@
     it; \
 })
 
-// Requires GNUC:
 #define FIND_IF_ITERATOR(first, last, predicate) ({ \
     auto it = (first); \
     for (; it < last && !(predicate)(*it); ++it) { \
@@ -81,7 +73,6 @@
     it; \
 })
 
-// Requires GNUC:
 #define FIND_IF_NOT(range, predicate) ({ \
     auto it = (range).data; \
     for (; it != END_POINTER(range) && (predicate)(*it); ++it) { \
@@ -89,7 +80,6 @@
     it; \
 })
 
-// Requires GNUC:
 #define FIND_IF_BACKWARDS(range, predicate) ({ \
     auto it = END_POINTER(range) - 1; \
     for (; it != (range).data - 1 && !(predicate)(*it); --it) { \
@@ -97,7 +87,6 @@
     it; \
 })
 
-// Requires GNUC:
 #define FIND_IF_NOT_BACKWARDS(range, predicate) ({ \
     auto it = END_POINTER(range) - 1; \
     for (; it != (range).data - 1 && (predicate)(*it); --it) { \
@@ -105,7 +94,6 @@
     it; \
 })
 
-// Requires GNUC:
 #define FIND_IF_NOT_BACKWARDS_ITERATOR(first, last, predicate) ({ \
     auto it = (last) - 1; \
     for (; it != (range).data - 1 && (predicate)(*it); --it) { \
@@ -116,7 +104,6 @@
 ////////////////////////////////////////////////////////////////////////////////
 // DYNAMIC ARRAY ALGORITHMS
 
-// Requires GNUC:
 // TODO: think about capacity when calling this with a darray.
 #define DROP_WHILE(range, predicate) ({ \
     auto result = (range); \
@@ -125,7 +112,6 @@
     result; \
 })
 
-// Requires GNUC:
 // TODO: think about capacity when calling this with a darray.
 #define DROP_UNTIL(range, predicate) ({ \
     auto result = (range); \
@@ -134,7 +120,6 @@
     result; \
 })
 
-// Requires GNUC:
 #define DROP_BACK_WHILE(range, predicate) ({ \
     auto result = (range); \
     for (; (result).count > 0 && (predicate)(*(END_POINTER(result) - 1)); --result.count) { \
@@ -142,7 +127,6 @@
     result; \
 })
 
-// Requires GNUC:
 #define DROP_BACK_UNTIL(range, predicate) ({ \
     auto result = (range); \
     for (; (result).count > 0 && !(predicate)(*(END_POINTER(result) - 1)); --result.count) { \
