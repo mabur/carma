ca0f1be83d9a41a3ff65c0d6ae0708cd61259e32
Magnus Burenius <magnus.burenius@gmail.com>
2024-07-12 15:07:12 +0200

    refactor: format string tests

--- carma_examples/tests.c
+++ carma_examples/tests.c
@@ -76,6 +76,18 @@ int global_assert_errors = 0;
     } \
 } while (0);
 
+void ASSERT_DYNAMIC_STRING(
+    const char* description,
+    DynamicString string,
+    const char* data,
+    size_t count,
+    size_t capacity
+) {
+    ASSERT_EQUAL_STRINGS((description), (string).data, (data));
+    ASSERT_EQUAL((description), (string).count, (count));
+    ASSERT_EQUAL((description), (string).capacity, (capacity));
+}
+
 void summarize_tests() {
     if (global_assert_errors != 0) {
         printf("%d/%d test failed\n", global_assert_errors, global_assert_count);
@@ -495,50 +507,35 @@ void test_format_string() {
     
     s = (DynamicString){};
     FORMAT_STRING(s, "");
-    ASSERT_EQUAL_STRINGS("test_format_string 0", s.data, "");
-    ASSERT_EQUAL("test_format_string 0 count", s.count, 0);
-    ASSERT_EQUAL("test_format_string 0 capacity", s.capacity, 1);
+    ASSERT_DYNAMIC_STRING("test_format_string 0", s, "", 0, 1);
 
     s = (DynamicString){};
     FORMAT_STRING(s, "a");
-    ASSERT_EQUAL_STRINGS("test_format_string 1", s.data, "a");
-    ASSERT_EQUAL("test_format_string 1 count", s.count, 1);
-    ASSERT_EQUAL("test_format_string 1 capacity", s.capacity, 2);
+    ASSERT_DYNAMIC_STRING("test_format_string 1", s, "a", 1, 2);
 
     s = (DynamicString){};
     FORMAT_STRING(s, "ab");
-    ASSERT_EQUAL_STRINGS("test_format_string 2", s.data, "ab");
-    ASSERT_EQUAL("test_format_string 2 count", s.count, 2);
-    ASSERT_EQUAL("test_format_string 2 capacity", s.capacity, 4);
+    ASSERT_DYNAMIC_STRING("test_format_string 2", s, "ab", 2, 4);
 
     s = (DynamicString){};
     FORMAT_STRING(s, "%i%s\n%i", 1, "ab", 99);
-    ASSERT_EQUAL_STRINGS("test_format_string 6", s.data, "1ab\n99");
-    ASSERT_EQUAL("test_format_string 6 count", s.count, 6);
-    ASSERT_EQUAL("test_format_string 6 capacity", s.capacity, 8);
+    ASSERT_DYNAMIC_STRING("test_format_string 3", s, "1ab\n99", 6, 8);
 
     s = (DynamicString){};
     FORMAT_STRING(s, "a");
     FORMAT_STRING(s, "b");
-    ASSERT_EQUAL_STRINGS("test_format_string 7", s.data, "ab");
-    ASSERT_EQUAL("test_format_string 7 count", s.count, 2);
-    ASSERT_EQUAL("test_format_string 7 capacity", s.capacity, 4);
-
+    ASSERT_DYNAMIC_STRING("test_format_string 4", s, "ab", 2, 4);
 
     s = (DynamicString){};
     FORMAT_STRING(s, "");
     FORMAT_STRING(s, "");
-    ASSERT_EQUAL_STRINGS("test_format_string 8", s.data, "");
-    ASSERT_EQUAL("test_format_string 8 count", s.count, 0);
-    ASSERT_EQUAL("test_format_string 8 capacity", s.capacity, 1);
+    ASSERT_DYNAMIC_STRING("test_format_string 5", s, "", 0, 1);
     
     s = (DynamicString){};
     FORMAT_STRING(s, "a");
     FORMAT_STRING(s, "bb");
     FORMAT_STRING(s, "ccc");
-    ASSERT_EQUAL_STRINGS("test_format_string 9", s.data, "abbccc");
-    ASSERT_EQUAL("test_format_string 9 count", s.count, 6);
-    ASSERT_EQUAL("test_format_string 9 capacity", s.capacity, 8);
+    ASSERT_DYNAMIC_STRING("test_format_string 6", s, "abbccc", 6, 8);
 }
 
 int main() {
