2171df0186a6cbe677c2d20f17131d704d6af55c
Magnus Burenius <magnus.burenius@gmail.com>
2024-09-25 21:20:41 +0200

    Update words.c

--- carma_examples/words.c
+++ carma_examples/words.c
@@ -1,5 +1,24 @@
 #include <stdio.h>
 #include <carma/carma.h>
+#include <carma/carma_table.h>
+
+typedef struct {
+    char* data;
+    size_t count;
+    size_t capacity;
+} String;
+
+typedef struct {
+    String keys;
+    size_t value;
+    bool occupied;
+} Item;
+
+typedef struct {
+    Item* data;
+    size_t count;
+    size_t capacity;
+} Table;
 
 int main(int argc, char **argv) {
     if (argc < 2) {
@@ -7,7 +26,21 @@ int main(int argc, char **argv) {
     }
     auto file_path = argv[1];
     auto count = 0;
+    auto word = (String){};
+    auto table = (Table){};
     READ_LINES(line, 255, file_path) {
+
+        RESERVE(word, strlen(line));
+        // Copy line to word.
+        // Split line into word.
+        // Increase count for word in table.
+
+        auto word_count = 0;
+        FIND_KEYS(word, value_it, table) {
+            word_count = *value_it;
+        }
+        SET_KEYS_VALUE(word, word_count, table);
+        
         count++;
     }
     printf("The file %s contains %d lines\n", file_path, count);
