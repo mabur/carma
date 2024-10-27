2237765d9fcd28d3d60e12642f48f90994d05329
Magnus Burenius <magnus.burenius@gmail.com>
2024-10-27 20:58:11 +0100

    Update words.c

--- carma_examples/words.c
+++ carma_examples/words.c
@@ -55,5 +55,23 @@ int main(int argc, char **argv) {
         printf("%.*s\n", (int)part.count, part.data);
     }
     printf("END\n");
+    
+    auto text = read_text_file(file_path);
+    auto text_view = (ConstantString){.data = text.data, .count=text.count};
+    printf("%.*s\n", (int)text_view.count, text_view.data);
+    printf("WORDS:\n");
+    auto word_count = 0;
+    FOR_EACH_WORD(part, text_view, isspace) {
+        ++word_count;
+        //printf("%.*s\n", (int)part.count, part.data);
+        printf("Word length: %zu\n", part.count);
+        FOR_EACH(c, part) {
+            putchar(*c);
+            
+        }
+        printf("\n");
+    }
+    printf("word_count=%d\n", word_count);
+    
     return EXIT_SUCCESS;
 }
