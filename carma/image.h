7c6b9f52f46d800633ae32a4609b4b7048498101
Magnus Burenius <magnus.burenius@gmail.com>
2025-06-08 17:17:13 +0200

    add FLIP_IMAGE_X FLIP_IMAGE_Y

---
+++ carma/image.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#define SWAP(a, b) do { \
+    auto c = (a); \
+    (a) = (b); \
+    (b) = c; \
+} while (0)
+
+#define FLIP_IMAGE_X(image) do { \
+    auto width = (image).width; \
+    auto height = (image).height; \
+    for (typeof(height) y = 0; y < height; ++y) \
+        for (typeof(width) x = 0; x < width / 2; ++x) \
+            SWAP((image).data[y * width + width - 1 - x], (image).data[y * width + x]); \
+} while(0)
+
+#define FLIP_IMAGE_Y(image) do { \
+    auto width = (image).width; \
+    auto height = (image).height; \
+    for (typeof(height) y = 0; y < height / 2; ++y) \
+        for (typeof(width) x = 0; x < width; ++x) \
+            SWAP((image).data[(height - 1 - y) * width + x], (image).data[y * width + x]); \
+} while(0)
