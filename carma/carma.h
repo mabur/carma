e16543fa8679a49f854fd5495e7609b77f821ee2
Magnus Burenius <magnus.burenius@gmail.com>
2025-06-08 17:20:27 +0200

    move FLIP_IMAGE_* macros

--- carma/carma.h
+++ carma/carma.h
@@ -288,3 +288,19 @@ static inline bool carma_are_bits_equal(
 
 #define AT_XYZ(array, x, y, z) \
     ((array).data[(array).height * (array).width * (z) + (array).width * (y) + (x)])
+
+#define FLIP_IMAGE_X(image) do { \
+    auto width = (image).width; \
+    auto height = (image).height; \
+    for (typeof(height) y = 0; y < height; ++y) \
+        for (typeof(width) x = 0; x < width / 2; ++x) \
+            SWAP((image).data[y * width + width - 1 - x], (image).data[y * width + x]); \
+} while(0)
+
+#define FLIP_IMAGE_Y(image) do { \
+    auto width = (image).width; \
+    auto height = (image).height; \
+    for (typeof(height) y = 0; y < height / 2; ++y) \
+        for (typeof(width) x = 0; x < width; ++x) \
+            SWAP((image).data[(height - 1 - y) * width + x], (image).data[y * width + x]); \
+} while(0)
