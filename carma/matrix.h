a82cd9c32a6c5547364b956a7de29ee194e23108
Magnus Burenius <magnus.burenius@gmail.com>
2025-09-13 16:44:46 +0200

    add inverse_float4x4

--- carma/matrix.h
+++ carma/matrix.h
@@ -84,3 +84,48 @@ inline float3x3 inverse_float3x3(float3x3 A) {
     R.columns[2] = (float3){ C02, C12, C22 } * inv_det;
     return R;
 }
+
+inline float4x4 inverse_float4x4(float4x4 A) {
+    float4 c0 = A.columns[0]; // {a00, a10, a20, a30}
+    float4 c1 = A.columns[1]; // {a01, a11, a21, a31}
+    float4 c2 = A.columns[2]; // {a02, a12, a22, a32}
+    float4 c3 = A.columns[3]; // {a03, a13, a23, a33}
+
+    float a00 = c0[0], a10 = c0[1], a20 = c0[2], a30 = c0[3];
+    float a01 = c1[0], a11 = c1[1], a21 = c1[2], a31 = c1[3];
+    float a02 = c2[0], a12 = c2[1], a22 = c2[2], a32 = c2[3];
+    float a03 = c3[0], a13 = c3[1], a23 = c3[2], a33 = c3[3];
+
+    // Cofactors Cij (unrolled). Column-major output later uses
+    // column j = {C0j, C1j, C2j, C3j}.
+    float C00 =  a11*a22*a33 - a11*a23*a32 - a21*a12*a33 + a21*a13*a32 + a31*a12*a23 - a31*a13*a22;
+    float C01 = -a01*a22*a33 + a01*a23*a32 + a21*a02*a33 - a21*a03*a32 - a31*a02*a23 + a31*a03*a22;
+    float C02 =  a01*a12*a33 - a01*a13*a32 - a11*a02*a33 + a11*a03*a32 + a31*a02*a13 - a31*a03*a12;
+    float C03 = -a01*a12*a23 + a01*a13*a22 + a11*a02*a23 - a11*a03*a22 - a21*a02*a13 + a21*a03*a12;
+
+    float C10 = -a10*a22*a33 + a10*a23*a32 + a20*a12*a33 - a20*a13*a32 - a30*a12*a23 + a30*a13*a22;
+    float C11 =  a00*a22*a33 - a00*a23*a32 - a20*a02*a33 + a20*a03*a32 + a30*a02*a23 - a30*a03*a22;
+    float C12 = -a00*a12*a33 + a00*a13*a32 + a10*a02*a33 - a10*a03*a32 - a30*a02*a13 + a30*a03*a12;
+    float C13 =  a00*a12*a23 - a00*a13*a22 - a10*a02*a23 + a10*a03*a22 + a20*a02*a13 - a20*a03*a12;
+
+    float C20 =  a10*a21*a33 - a10*a23*a31 - a20*a11*a33 + a20*a13*a31 + a30*a11*a23 - a30*a13*a21;
+    float C21 = -a00*a21*a33 + a00*a23*a31 + a20*a01*a33 - a20*a03*a31 - a30*a01*a23 + a30*a03*a21;
+    float C22 =  a00*a11*a33 - a00*a13*a31 - a10*a01*a33 + a10*a03*a31 + a30*a01*a13 - a30*a03*a11;
+    float C23 = -a00*a11*a23 + a00*a13*a21 + a10*a01*a23 - a10*a03*a21 - a20*a01*a13 + a20*a03*a11;
+
+    float C30 = -a10*a21*a32 + a10*a22*a31 + a20*a11*a32 - a20*a12*a31 - a30*a11*a22 + a30*a12*a21;
+    float C31 =  a00*a21*a32 - a00*a22*a31 - a20*a01*a32 + a20*a02*a31 + a30*a01*a22 - a30*a02*a21;
+    float C32 = -a00*a11*a32 + a00*a12*a31 + a10*a01*a32 - a10*a02*a31 - a30*a01*a12 + a30*a02*a11;
+    float C33 =  a00*a11*a22 - a00*a12*a21 - a10*a01*a22 + a10*a02*a21 + a20*a01*a12 - a20*a02*a11;
+
+    // Determinant via first row expansion: det = a00*C00 + a01*C10 + a02*C20 + a03*C30
+    float det = a00*C00 + a01*C10 + a02*C20 + a03*C30;
+    float inv_det = 1.0f / det;
+
+    float4x4 R;
+    R.columns[0] = (float4){ C00, C10, C20, C30 } * inv_det;
+    R.columns[1] = (float4){ C01, C11, C21, C31 } * inv_det;
+    R.columns[2] = (float4){ C02, C12, C22, C32 } * inv_det;
+    R.columns[3] = (float4){ C03, C13, C23, C33 } * inv_det;
+    return R;
+}
