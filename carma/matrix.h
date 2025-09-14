008c5870b3e88c4c4ba2c287f38ed2c41a4f7e6a
Magnus Burenius <magnus.burenius@gmail.com>
2025-09-14 16:20:11 +0200

    add inverse_double4x4

--- carma/matrix.h
+++ carma/matrix.h
@@ -129,39 +129,81 @@ inline double3x3 inverse_double3x3(double3x3 A) {
     return result;
 }
 
+#define SET_INVERSE_4x4(in, out) do { \
+    auto a00 = (in).columns[0][0]; \
+    auto a01 = (in).columns[1][0]; \
+    auto a02 = (in).columns[2][0]; \
+    auto a03 = (in).columns[3][0]; \
+    auto a10 = (in).columns[0][1]; \
+    auto a11 = (in).columns[1][1]; \
+    auto a12 = (in).columns[2][1]; \
+    auto a13 = (in).columns[3][1]; \
+    auto a20 = (in).columns[0][2]; \
+    auto a21 = (in).columns[1][2]; \
+    auto a22 = (in).columns[2][2]; \
+    auto a23 = (in).columns[3][2]; \
+    auto a30 = (in).columns[0][3]; \
+    auto a31 = (in).columns[1][3]; \
+    auto a32 = (in).columns[2][3]; \
+    auto a33 = (in).columns[3][3]; \
+    \
+    auto C00 =  (a11*(a22*a33 - a23*a32) - a12*(a21*a33 - a23*a31) + a13*(a21*a32 - a22*a31)); \
+    auto C01 = -(a10*(a22*a33 - a23*a32) - a12*(a20*a33 - a23*a30) + a13*(a20*a32 - a22*a30)); \
+    auto C02 =  (a10*(a21*a33 - a23*a31) - a11*(a20*a33 - a23*a30) + a13*(a20*a31 - a21*a30)); \
+    auto C03 = -(a10*(a21*a32 - a22*a31) - a11*(a20*a32 - a22*a30) + a12*(a20*a31 - a21*a30)); \
+    \
+    auto C10 = -(a01*(a22*a33 - a23*a32) - a02*(a21*a33 - a23*a31) + a03*(a21*a32 - a22*a31)); \
+    auto C11 =  (a00*(a22*a33 - a23*a32) - a02*(a20*a33 - a23*a30) + a03*(a20*a32 - a22*a30)); \
+    auto C12 = -(a00*(a21*a33 - a23*a31) - a01*(a20*a33 - a23*a30) + a03*(a20*a31 - a21*a30)); \
+    auto C13 =  (a00*(a21*a32 - a22*a31) - a01*(a20*a32 - a22*a30) + a02*(a20*a31 - a21*a30)); \
+    \
+    auto C20 =  (a01*(a12*a33 - a13*a32) - a02*(a11*a33 - a13*a31) + a03*(a11*a32 - a12*a31)); \
+    auto C21 = -(a00*(a12*a33 - a13*a32) - a02*(a10*a33 - a13*a30) + a03*(a10*a32 - a12*a30)); \
+    auto C22 =  (a00*(a11*a33 - a13*a31) - a01*(a10*a33 - a13*a30) + a03*(a10*a31 - a11*a30)); \
+    auto C23 = -(a00*(a11*a32 - a12*a31) - a01*(a10*a32 - a12*a30) + a02*(a10*a31 - a11*a30)); \
+    \
+    auto C30 = -(a01*(a12*a23 - a13*a22) - a02*(a11*a23 - a13*a21) + a03*(a11*a22 - a12*a21)); \
+    auto C31 =  (a00*(a12*a23 - a13*a22) - a02*(a10*a23 - a13*a20) + a03*(a10*a22 - a12*a20)); \
+    auto C32 = -(a00*(a11*a23 - a13*a21) - a01*(a10*a23 - a13*a20) + a03*(a10*a21 - a11*a20)); \
+    auto C33 =  (a00*(a11*a22 - a12*a21) - a01*(a10*a22 - a12*a20) + a02*(a10*a21 - a11*a20)); \
+    \
+    auto det = a00*C00 + a01*C01 + a02*C02 + a03*C03; \
+    auto inv_det = (1) / det; \
+    \
+    (out).columns[0][0] = C00; \
+    (out).columns[1][0] = C01; \
+    (out).columns[2][0] = C02; \
+    (out).columns[3][0] = C03; \
+    \
+    (out).columns[0][1] = C10; \
+    (out).columns[1][1] = C11; \
+    (out).columns[2][1] = C12; \
+    (out).columns[3][1] = C13; \
+    \
+    (out).columns[0][2] = C20; \
+    (out).columns[1][2] = C21; \
+    (out).columns[2][2] = C22; \
+    (out).columns[3][2] = C23; \
+    \
+    (out).columns[0][3] = C30; \
+    (out).columns[1][3] = C31; \
+    (out).columns[2][3] = C32; \
+    (out).columns[3][3] = C33; \
+    \
+    (out).columns[0] *= inv_det; \
+    (out).columns[1] *= inv_det; \
+    (out).columns[2] *= inv_det; \
+    (out).columns[3] *= inv_det; \
+} while (0)
+
 inline float4x4 inverse_float4x4(float4x4 A) {
-    float a00 = A.columns[0][0], a01 = A.columns[1][0], a02 = A.columns[2][0], a03 = A.columns[3][0];
-    float a10 = A.columns[0][1], a11 = A.columns[1][1], a12 = A.columns[2][1], a13 = A.columns[3][1];
-    float a20 = A.columns[0][2], a21 = A.columns[1][2], a22 = A.columns[2][2], a23 = A.columns[3][2];
-    float a30 = A.columns[0][3], a31 = A.columns[1][3], a32 = A.columns[2][3], a33 = A.columns[3][3];
+    float4x4 result;
+    SET_INVERSE_4x4(A, result);
+    return result;
+}
 
-    float C00 =  a11*a22*a33 - a11*a23*a32 - a21*a12*a33 + a21*a13*a32 + a31*a12*a23 - a31*a13*a22;
-    float C01 = -a01*a22*a33 + a01*a23*a32 + a21*a02*a33 - a21*a03*a32 - a31*a02*a23 + a31*a03*a22;
-    float C02 =  a01*a12*a33 - a01*a13*a32 - a11*a02*a33 + a11*a03*a32 + a31*a02*a13 - a31*a03*a12;
-    float C03 = -a01*a12*a23 + a01*a13*a22 + a11*a02*a23 - a11*a03*a22 - a21*a02*a13 + a21*a03*a12;
-
-    float C10 = -a10*a22*a33 + a10*a23*a32 + a20*a12*a33 - a20*a13*a32 - a30*a12*a23 + a30*a13*a22;
-    float C11 =  a00*a22*a33 - a00*a23*a32 - a20*a02*a33 + a20*a03*a32 + a30*a02*a23 - a30*a03*a22;
-    float C12 = -a00*a12*a33 + a00*a13*a32 + a10*a02*a33 - a10*a03*a32 - a30*a02*a13 + a30*a03*a12;
-    float C13 =  a00*a12*a23 - a00*a13*a22 - a10*a02*a23 + a10*a03*a22 + a20*a02*a13 - a20*a03*a12;
-
-    float C20 =  a10*a21*a33 - a10*a23*a31 - a20*a11*a33 + a20*a13*a31 + a30*a11*a23 - a30*a13*a21;
-    float C21 = -a00*a21*a33 + a00*a23*a31 + a20*a01*a33 - a20*a03*a31 - a30*a01*a23 + a30*a03*a21;
-    float C22 =  a00*a11*a33 - a00*a13*a31 - a10*a01*a33 + a10*a03*a31 + a30*a01*a13 - a30*a03*a11;
-    float C23 = -a00*a11*a23 + a00*a13*a21 + a10*a01*a23 - a10*a03*a21 - a20*a01*a13 + a20*a03*a11;
-
-    float C30 = -a10*a21*a32 + a10*a22*a31 + a20*a11*a32 - a20*a12*a31 - a30*a11*a22 + a30*a12*a21;
-    float C31 =  a00*a21*a32 - a00*a22*a31 - a20*a01*a32 + a20*a02*a31 + a30*a01*a22 - a30*a02*a21;
-    float C32 = -a00*a11*a32 + a00*a12*a31 + a10*a01*a32 - a10*a02*a31 - a30*a01*a12 + a30*a02*a11;
-    float C33 =  a00*a11*a22 - a00*a12*a21 - a10*a01*a22 + a10*a02*a21 + a20*a01*a12 - a20*a02*a11;
-
-    float det = a00*C00 + a01*C10 + a02*C20 + a03*C30;
-    float inv_det = 1.0f / det;
-
-    float4x4 R;
-    R.columns[0] = (float4){ C00, C10, C20, C30 } * inv_det;
-    R.columns[1] = (float4){ C01, C11, C21, C31 } * inv_det;
-    R.columns[2] = (float4){ C02, C12, C22, C32 } * inv_det;
-    R.columns[3] = (float4){ C03, C13, C23, C33 } * inv_det;
-    return R;
+inline double4x4 inverse_double4x4(double4x4 A) {
+    double4x4 result;
+    SET_INVERSE_4x4(A, result);
+    return result;
 }
