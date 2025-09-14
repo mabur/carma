7a87ab7404e229fbdecda65430b734f793ee0ea2
Magnus Burenius <magnus.burenius@gmail.com>
2025-09-14 15:13:29 +0200

    fix let macro

--- carma/make.h
+++ carma/make.h
@@ -1,18 +1,44 @@
 #pragma once
 
 // Define a "let" macro for type inference,
-// that is easy to type and works across C/C++ versions:
-#if defined(__cplusplus) && __cplusplus >= 201103L
+// that is easy to type and works across C/C++ versions.
+// Try to use C++11 auto:
+#if defined(__cplusplus)
+  #if __cplusplus >= 201103L
+
     #define let auto
-#elif defined(__STDC_VERSION__) && __STDC_VERSION__ >= 202311L
-    #define let auto
-#elif defined(__clang__) && __has_extension(__auto_type)
-    #define let __auto_type
-#elif defined(__GNUC__) && (__GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 9))
-    #define let __auto_type
-#else
-    #error "The 'let' macro requires C++11, C23, or compiler support for __auto_type"
+  #endif
 #endif
+// Try to use C23 auto:
+#ifndef let
+  #if defined(__STDC_VERSION__)
+    #if __STDC_VERSION__ >= 202311L
+      #define let auto
+    #endif
+  #endif
+#endif
+// Try to use Clang __auto_type
+#ifndef let
+#if defined(__clang__)
+  #if defined(__has_extension)
+    #if __has_extension(__auto_type)
+      #define let __auto_type
+    #endif
+  #endif
+#endif
+#endif
+// Try to use GCC __auto_type:
+#ifndef let
+#if defined(__GNUC__)
+  #if (__GNUC__ > 4) || (__GNUC__ == 4 && __GNUC_MINOR__ >= 9)
+    #define let __auto_type
+  #endif
+#endif
+#endif
+#ifndef let
+#error "The 'let' macro requires C++11, C23, or compiler support for __auto_type"
+#endif
+
 
 // Define a "make" macro for making "struct literals",
 // that is easy to type and works across C/C++ versions.
