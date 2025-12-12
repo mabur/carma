f82bc2b0b9c410e0a031e6921ac765d1a7ef8092
Magnus Burenius <magnus.burenius@gmail.com>
2025-12-12 20:41:37 +0100

    refactor: CARMA_AUTO  definition

--- carma/carma_type_inference.h
+++ carma/carma_type_inference.h
@@ -1,17 +1,11 @@
 #pragma once
 
-#if defined(__cplusplus)
-    #if __cplusplus >= 201103L
-        #define CARMA_AUTO auto
-    #else
-        #error "CARMA_AUTO requires any of these: C23, C++11, GCC, Clang"
-    #endif
+#if defined(__cplusplus) && __cplusplus >= 201103L
+    #define CARMA_AUTO auto
+#elif defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 202311L)
+    #define CARMA_AUTO auto
+#elif defined(__GNUC__)
+    #define CARMA_AUTO __auto_type
 #else
-    #if defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 202311L)
-        #define CARMA_AUTO auto
-    #elif defined(__clang__) || defined(__GNUC__)
-        #define CARMA_AUTO __auto_type
-    #else
-        #error "CARMA_AUTO requires any of these: C23, C++11, GCC, Clang"
-    #endif
+    #error "CARMA_AUTO requires any of these: C++11, C23, GCC, Clang"
 #endif
