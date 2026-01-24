a7993fa4b849f840034dc4861e809f339892d936
Magnus Burenius <magnus.burenius@gmail.com>
2026-01-24 14:28:29 +0100

    Create carma_serialize.h

---
+++ carma/carma_serialize.h
@@ -0,0 +1,86 @@
+#pragma once
+
+/*
+
+* Use Generic to overload a single serialization function?
+  - bool/true/false/int gotchas.
+* serialize vs format
+* Have OpenGL like state machine, that checks grammar and manages indendation?
+
+These concat to s:
+serialize_indentation(&s, 4);
+serialize_newline(&s, x);
+serialize_string(&s, x);
+serialize_char(&s, x);
+serialize_int(&s, x);
+serialize_double(&s, x);
+serialize_bool(&s, x);
+serialize_quoted_string(&s, x);
+serialize_json_list_begin(&s);
+serialize_json_list_end(&s);
+serialize_json_object_begin(&s);
+serialize_json_object_key(&s, "key");
+serialize_json_object_end(&s);
+
+auto s = (StringView){};
+serialize_json_object_begin(&s);
+    serialize_json_object_key(&s, "visible");
+    serialize_bool(&s, true);
+    serialize_json_object_key(&s, "width");
+    serialize_int(&s, 640);
+    serialize_json_object_key(&s, "height");
+    serialize_int(&s, 480);
+    serialize_json_object_key(&s, "pixel");
+    serialize_json_list_begin(&s);
+        serialize_json_list_item(&s);
+        serialize_int(&s, 0xFF00FF);
+    serialize_json_list_end(&s);
+serialize_json_object_end(&s);
+
+auto j = (JsonBuilder){};
+BEGIN_JSON_OBJECT(j);
+    ADD_JSON_KEY(j, "visible");
+    ADD_JSON_BOOL(j, true);
+    ADD_JSON_KEY(j, "width");
+    ADD_JSON_INT(j, 640);
+    ADD_JSON_KEY(j, "height");
+    ADD_JSON_INT(j, 480);
+    ADD_JSON_KEY(j, "pixels");
+    BEGIN_JSON_LIST(j);
+        ADD_JSON_INT(j, 0xFF00FF);
+    END_JSON_LIST(j);
+END_JSON_OBJECT(j);
+
+auto j = (JsonBuilder){};
+BEGIN_JSON_OBJECT(j);
+{
+    ADD_JSON_KEY(j, "visible");
+    ADD_JSON_BOOL(j, true);
+    ADD_JSON_KEY(j, "width");
+    ADD_JSON_INT(j, 640);
+    ADD_JSON_KEY(j, "height");
+    ADD_JSON_INT(j, 480);
+    ADD_JSON_KEY(j, "pixels");
+    BEGIN_JSON_LIST(j);
+    {
+        ADD_JSON_INT(j, 0xFF00FF);
+    }
+    END_JSON_LIST(j);
+}
+END_JSON_OBJECT(j);
+
+auto j = (JsonBuilder){};
+ADD_JSON_OBJECT(j) {
+    ADD_JSON_KEY(j, "visible");
+    ADD_JSON_BOOL(j, true);
+    ADD_JSON_KEY(j, "width");
+    ADD_JSON_INT(j, 640);
+    ADD_JSON_KEY(j, "height");
+    ADD_JSON_INT(j, 480);
+    ADD_JSON_KEY(j, "pixels");
+    ADD_JSON_LIST(j) {
+        ADD_JSON_INT(j, 0xFF00FF);
+    }
+}
+
+*/
