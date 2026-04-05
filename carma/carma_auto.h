a1761211c6697442f5ae14425a6d0ee04f09470a
Magnus Burenius <magnus.burenius@gmail.com>
2026-04-05 12:48:15 +0200

    support MSVS2010

--- carma/carma_auto.h
+++ carma/carma_auto.h
@@ -1,6 +1,8 @@
 #pragma once
 
-#if defined(__cplusplus) && __cplusplus >= 201103L
+#if defined(_MSC_VER) && _MSC_VER >= 1600
+    #define CARMA_AUTO auto
+#elif defined(__cplusplus) && __cplusplus >= 201103L
     #define CARMA_AUTO auto
 #elif defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 202311L)
     #define CARMA_AUTO auto
