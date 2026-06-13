beeb3e08fd68c2bf26ddd96606e0901ecc897473
Magnus Burenius <magnus.burenius@gmail.com>
2026-06-13 12:32:31 +0200

    refactor: re-order string macros

--- carma/carma_string.h
+++ carma/carma_string.h
@@ -21,7 +21,9 @@ typedef struct StringBuilder {
     size_t capacity;
 } StringBuilder;
 
-#define STRING_VIEW(cstring) MAKE(StringView, (cstring), strlen(cstring))
+
+////////////////////////////////////////////////////////////////////////////////
+// GENERAL STRING MACROS
 
 static inline char* carma_make_cstring(const char* data, size_t count) {
     char* result = (char*)malloc(count + 1);
@@ -32,15 +34,18 @@ static inline char* carma_make_cstring(const char* data, size_t count) {
     return result;
 }
 
-static inline char* carma_as_cstring(StringBuilder* string_builder) {
-    APPEND(*string_builder, '\0');
-    DROP_BACK(*string_builder);
-    return string_builder->data;
-}
-
 #define MAKE_CSTRING(string) carma_make_cstring((string).data, (string).count)
 
-#define AS_CSTRING(string_builder) carma_as_cstring(&(string_builder))
+#define PRINT_CARMA_STRING(string) do { \
+    FOR_EACH(c, (string)) { \
+        putchar(*c); \
+    } \
+} while (0)
+
+////////////////////////////////////////////////////////////////////////////////
+// STRING VIEW MACROS
+
+#define STRING_VIEW(cstring) MAKE(StringView, (cstring), strlen(cstring))
 
 static inline
 StringBuilder carma_vconcat_string(StringBuilder string, const char* format, va_list args) {
@@ -123,38 +128,15 @@ size_t carma_find_first_character_not_of(const char* data, size_t max_index, int
     )
 
 ////////////////////////////////////////////////////////////////////////////////
-// PRINTING
+// STRING BUILDER MACROS
 
-#define PRINT_CARMA_STRING(string) do { \
-    FOR_EACH(c, (string)) { \
-        putchar(*c); \
-    } \
-} while (0)
-
-////////////////////////////////////////////////////////////////////////////////
-// FILE ALGORITHMS
-
-#define FOR_FILE(file, file_path, mode) for (CARMA_AUTO file = fopen((file_path), mode); (file); fclose(file), (file) = 0)
-
-#define FOR_LINES(line, capacity, file) for (char line[capacity]; fgets(line, (capacity), (file)) != NULL;)
-
-#define READ_LINES(line, capacity, file_path) \
-    FOR_FILE(_file, file_path, "r") \
-        FOR_LINES(line, capacity, _file)
-
-static inline
-StringBuilder read_text_file(const char* file_path) {
-    StringBuilder result = {};
-    FOR_FILE(file, file_path, "r") {
-        for (int ch; ch = fgetc(file), ch != EOF;) {
-            APPEND(result, (char)ch);
-        }
-    }
-    return result;
+static inline char* carma_as_cstring(StringBuilder* string_builder) {
+    APPEND(*string_builder, '\0');
+    DROP_BACK(*string_builder);
+    return string_builder->data;
 }
 
-////////////////////////////////////////////////////////////////////////////////
-/// Serialization
+#define AS_CSTRING(string_builder) carma_as_cstring(&(string_builder))
 
 #define SERIALIZE_INTEGRAL(string_builder, x) do { \
     CARMA_AUTO _si_x = (x); \
@@ -219,3 +201,25 @@ StringBuilder read_text_file(const char* file_path) {
     CONCAT((string_builder), tail_with_null_terminator); \
     DROP_BACK(string_builder); \
 } while (0)
+
+////////////////////////////////////////////////////////////////////////////////
+// FILE ALGORITHMS
+
+#define FOR_FILE(file, file_path, mode) for (CARMA_AUTO file = fopen((file_path), mode); (file); fclose(file), (file) = 0)
+
+#define FOR_LINES(line, capacity, file) for (char line[capacity]; fgets(line, (capacity), (file)) != NULL;)
+
+#define READ_LINES(line, capacity, file_path) \
+    FOR_FILE(_file, file_path, "r") \
+        FOR_LINES(line, capacity, _file)
+
+static inline
+StringBuilder read_text_file(const char* file_path) {
+    StringBuilder result = {};
+    FOR_FILE(file, file_path, "r") {
+        for (int ch; ch = fgetc(file), ch != EOF;) {
+            APPEND(result, (char)ch);
+        }
+    }
+    return result;
+}
