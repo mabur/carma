af2622b5dd46470dc81bcd625182cda0b629ed78
Magnus Burenius <magnus.burenius@gmail.com>
2026-06-14 18:21:35 +0200

    Update day02_part1.c

--- carma_examples/advent_of_code_2025/day02_part1.c
+++ carma_examples/advent_of_code_2025/day02_part1.c
@@ -4,19 +4,19 @@
 #include <carma/carma_string.h>
 
 typedef struct Interval {
-    int64_t first;
-    int64_t last;
+    uint64_t first;
+    uint64_t last;
 } Interval;
 
 Interval parseInterval(StringView word) {
     auto interval = (Interval){};
-    sscanf(word.data, "%" SCNd64 "-%" SCNd64, &interval.first, &interval.last);
+    sscanf(word.data, "%" SCNu64 "-%" SCNu64, &interval.first, &interval.last);
     return interval;
 }
 
-bool isInvalidId(int64_t id) {
+bool isInvalidId(uint64_t id) {
     char s[32];
-    snprintf(s, sizeof(s), "%" PRId64, id);
+    snprintf(s, sizeof(s), "%" PRIu64, id);
     auto count = strlen(s);
     if (count % 2 != 0) {
         return false;
@@ -31,14 +31,14 @@ int main() {
     auto file_path = "day02.txt";
     auto text = read_text_file(file_path);
     APPEND(text, '\0');
-    int64_t sum = 0;
+    uint64_t sum = 0;
     FOR_EACH_WORD(word, text, ',') {
         auto interval = parseInterval(word);
-        for (int64_t id = interval.first; id <= interval.last; id++) {
+        for (uint64_t id = interval.first; id <= interval.last; id++) {
             if (isInvalidId(id)) {
                 sum += id;
             }
         }
     }
-    printf("%" PRId64 "\n", sum);
+    printf("%" PRIu64 "\n", sum);
 }
