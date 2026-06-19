aa0095e610be4be05eab1d5f0ca38149d49a79ae
Magnus Burenius <magnus.burenius@gmail.com>
2026-06-19 14:51:32 +0200

    fix: std lib includes

--- carma/carma_std.h
+++ carma/carma_std.h
@@ -1,21 +1,5 @@
 #pragma once
 
-#ifdef __cplusplus
-#include <limits.h>
-#include <climits>
-#include <stdarg.h>
-#include <cstdarg>
-#include <stddef.h>
-#include <cstddef>
-#include <stdint.h>
-#include <cstdint>
-#include <stdio.h>
-#include <cstdio>
-#include <stdlib.h>
-#include <cstdlib>
-#include <string.h>
-#include <cstring>
-#else
 #include <limits.h>
 #include <stdarg.h>
 #include <stddef.h>
@@ -24,4 +8,3 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
-#endif
