ef9f438de530791d6db6313cbd7d2c8a932bf579
Magnus Burenius <magnus.burenius@gmail.com>
2026-06-19 14:45:57 +0200

    fix: c++ std library includes for mingw

--- carma/carma_std.h
+++ carma/carma_std.h
@@ -1,12 +1,19 @@
 #pragma once
 
 #ifdef __cplusplus
+#include <limits.h>
 #include <climits>
+#include <stdarg.h>
 #include <cstdarg>
+#include <stddef.h>
 #include <cstddef>
+#include <stdint.h>
 #include <cstdint>
+#include <stdio.h>
 #include <cstdio>
+#include <stdlib.h>
 #include <cstdlib>
+#include <string.h>
 #include <cstring>
 #else
 #include <limits.h>
